6b62756d2b60532de6129183bd98d64a38f06ec8
bstriker <strikeforcezero@gmail.com>
2024-05-20 12:17:26 -0400

    fix(util): random_index_bias

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 use rand::rngs::ThreadRng;
-use rand_distr::{Distribution, Uniform};
+use statrs::distribution::{ContinuousCDF, Exp};
 
 /// Type alias for all probability / random usages
 pub type Odds = f64;
@@ -14,40 +14,46 @@ pub fn coin_flip(rng: &mut ThreadRng, odds: Odds) -> bool {
     rng.gen_bool(odds)
 }
 
+#[derive(Debug, Copy, Clone)]
 pub enum Bias {
     Front,
     End,
 }
 
-impl Bias {
-    fn to_pos_neg_f64(&self) -> f64 {
-        match self {
-            Self::Front => 1.0,
-            Self::End => -1.0,
-        }
-    }
+#[derive(Debug, Copy, Clone)]
+pub enum BiasMethod {
+    Cdf,
+    Sqrt,
 }
 
 fn extract_first_decimal(num: f64) -> u8 {
     ((f64::abs(num) * 10.0) % 10.0) as u8
 }
 
-fn find_bucket(num: f64, n: usize) -> usize {
-    let n = n as f64;
-    let num = f64::abs(num);
-    (num * n % n) as usize
-}
+/// Returns a random index from 0-len with a given bias
+pub fn random_index_bias(rng: &mut ThreadRng, len: usize, bias: Bias, method: BiasMethod) -> usize {
+    let x = rng.gen_range(0.0..=1.0);
 
-pub fn random_index_bias(rng: &mut ThreadRng, len: usize, bias: Bias) -> usize {
-    let lambda = 10.0; // rate parameter
-    let uniform = Uniform::new(0.0, 1.0); // uniform distribution on [0,1]
-    let y: f64 = uniform.sample(rng);
-    // y.ln() is the bias, -y.ln() end / +y.ln() front
-    let x: f64 = (bias.to_pos_neg_f64() * y.ln() / lambda) - 1.0;
-    let index = find_bucket(x, 10);
-    match bias {
-        Bias::Front => index,
-        Bias::End => len - index - 1,
+    match method {
+        BiasMethod::Cdf => {
+            let exp_dist = Exp::new(0.5).unwrap();
+            let cdf_val = exp_dist.cdf(x);
+            let value = (cdf_val * len as f64).floor() as usize;
+            match bias {
+                Bias::Front => value,
+                Bias::End => len - 1 - value,
+            }
+        }
+        BiasMethod::Sqrt => {
+            const BIAS: f64 = 2.0;
+            let value = 1f64 - x.powf(1f64 / BIAS).sqrt();
+            let value = value * len as f64;
+            let value = value.floor() as usize;
+            match bias {
+                Bias::Front => value,
+                Bias::End => len - 1 - value,
+            }
+        }
     }
 }
 
@@ -76,30 +82,45 @@ pub(crate) mod debug_tracing {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
-    use itertools::Itertools;
     use rand::thread_rng;
+    use rstest::rstest;
+    use tracing::debug;
 
     use super::*;
 
-    #[test]
-    fn test_cdf() {
-        let lambda = 10.0; // rate parameter
-        let uniform = Uniform::new(0.0, 1.0); // uniform distribution on [0,1]
-        let mut map: HashMap<usize, i32> = HashMap::new();
-        let mut rng = thread_rng();
-        for _ in 0..10000 {
-            let y: f64 = uniform.sample(&mut rng);
-            // y.ln() is the bias, -y.ln() end / +y.ln() front
-            let x: f64 = (-y.ln() / lambda) - 1.0;
-            let index = find_bucket(x, 10);
-
-            let entry = map.entry(index).or_default();
-            *entry += 1;
+    #[rstest(
+        input, expected,
+        case((100, Bias::End), |x| x > 70),
+        case((100, Bias::Front), |x| x < 30),
+    )]
+    /// There is at worst a 1/10^10 (1 in 10 billion) chance this fails
+    fn test_random_index_bias_cdf(input: (usize, Bias), expected: fn(usize) -> bool) {
+        let (input, bias) = input;
+        for _ in 0..100 {
+            let avg = (0..input)
+                .map(|_| random_index_bias(&mut thread_rng(), input, bias, BiasMethod::Cdf))
+                .sum::<usize>()
+                / input;
+            debug!("cdf {bias:?} {avg}");
+            assert!(expected(avg));
         }
-        for (k, v) in map.iter().sorted_by_key(|x| x.0) {
-            println!("{k}: {v}");
+    }
+
+    #[rstest(
+        input, expected,
+        case((100, Bias::End), |x| x > 70),
+        case((100, Bias::Front), |x| x < 30),
+    )]
+    /// There is at worst a 1/10^10 (1 in 10 billion) chance this fails
+    fn test_random_index_bias_sqrt(input: (usize, Bias), expected: fn(usize) -> bool) {
+        let (input, bias) = input;
+        for _ in 0..100 {
+            let avg = (0..input)
+                .map(|_| random_index_bias(&mut thread_rng(), input, bias, BiasMethod::Sqrt))
+                .sum::<usize>()
+                / input;
+            debug!("sqrt {bias:?} {avg}");
+            assert!(expected(avg));
         }
     }
 }
