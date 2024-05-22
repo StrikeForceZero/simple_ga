46b30b19b551aeb772fb6b4c0721e7e4de273009
bstriker <strikeforcezero@gmail.com>
2024-05-21 23:46:56 -0400

    chore(tests): Add tests to population

--- src/ga/population.rs
+++ src/ga/population.rs
@@ -73,3 +73,100 @@ impl<Subject: Hash + Eq + PartialEq> Population<Subject> {
         self.subjects.push(subject);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Range;
+
+    use rand::thread_rng;
+
+    use crate::ga::fitness::{Fitness, FitnessWrapped};
+    use crate::ga::population::Population;
+    use crate::ga::prune::PruneSingleSkipFirst;
+    use crate::ga::select::SelectRandomManyWithBias;
+    use crate::util::Bias;
+
+    fn test_subject(id: u32) -> FitnessWrapped<u32> {
+        FitnessWrapped::new(id, id as Fitness)
+    }
+
+    impl From<Range<u32>> for Population<u32> {
+        fn from(range: Range<u32>) -> Self {
+            Population {
+                pool_size: range.len(),
+                subjects: range.into_iter().map(test_subject).collect(),
+            }
+        }
+    }
+
+    fn make_population(size: usize) -> Population<u32> {
+        Population::from(0..size as u32)
+    }
+
+    #[test]
+    fn test_prune_random() {
+        let size = 3;
+        let mut population = make_population(size);
+        for n in 1..3 {
+            population.prune_random(PruneSingleSkipFirst, &mut thread_rng());
+            assert_eq!(population.subjects.len(), size - n);
+        }
+    }
+
+    #[test]
+    fn test_generic_select() {
+        let population = make_population(2);
+        for n in 0..=2 {
+            let selected = population.select_random(
+                &mut thread_rng(),
+                SelectRandomManyWithBias::new(n, Bias::Front),
+            );
+            assert_eq!(selected.len(), n);
+        }
+    }
+
+    #[test]
+    fn test_select_front() {
+        let population = make_population(2);
+        for n in 0..=2 {
+            let selected = population.select_front_bias_random(&mut thread_rng(), n);
+            assert_eq!(selected.len(), n);
+        }
+    }
+
+    #[test]
+    fn test_select_back() {
+        let population = make_population(2);
+        for n in 0..=2 {
+            let selected = population.select_back_bias_random(&mut thread_rng(), n);
+            assert_eq!(selected.len(), n);
+        }
+    }
+
+    #[test]
+    fn test_sort() {
+        let mut population = make_population(2);
+        population.subjects.insert(0, test_subject(3));
+        assert_eq!(
+            population.subjects,
+            vec![test_subject(3), test_subject(0), test_subject(1)]
+        );
+        population.sort();
+        assert_eq!(
+            population.subjects,
+            vec![test_subject(0), test_subject(1), test_subject(3)]
+        );
+    }
+
+    #[test]
+    fn test_add() {
+        let mut population = make_population(0);
+        for n in 1..3 {
+            population.add(test_subject(n));
+            assert_eq!(
+                population.subjects,
+                (1..=n).map(test_subject).collect::<Vec<_>>()
+            );
+        }
+    }
+}
