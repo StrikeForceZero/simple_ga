c38e47462cedd9b58afe8640f50251956807acc1
bstriker <strikeforcezero@gmail.com>
2024-10-03 13:56:37 -0400

    refactor: add support for boxed create subject fns

--- src/ga/inflate.rs
+++ src/ga/inflate.rs
@@ -12,12 +12,11 @@ pub trait InflateTarget {
 }
 
 #[derive(Debug, Copy, Clone, Default)]
-pub struct InflateUntilFull<F: ?Sized>(pub F);
+pub struct InflateUntilFull<CreateSubjectFunc: ?Sized>(pub CreateSubjectFunc);
 
-impl<Subject, CreateSubjectFunc> InflateTarget for InflateUntilFull<CreateSubjectFunc>
+impl<Subject> InflateTarget for InflateUntilFull<Box<dyn Fn(&GaContext) -> Subject>>
 where
     Subject: GaSubject + Hash + Eq + PartialEq + Fit<Fitness>,
-    CreateSubjectFunc: Fn(&GaContext) -> Subject,
 {
     type Params = GaContext;
     type Target = Population<Subject>;
@@ -25,19 +24,79 @@ where
         while target.subjects.len() < target.pool_size {
             // TODO: re-add warning if fitness = target fitness
             // this would require GeneticAlgorithmOptions to be passed in
-            target.add(self.0(params).into());
+            target.add((&self.0)(params).into());
         }
     }
 }
 
-impl<Subject, CreateSubjectFunc> GaAction for InflateUntilFull<CreateSubjectFunc>
+impl<Subject> GaAction for InflateUntilFull<Box<dyn Fn(&GaContext) -> Subject>>
 where
     Subject: GaSubject + Hash + Eq + PartialEq + Fit<Fitness>,
-    CreateSubjectFunc: Fn(&GaContext) -> Subject,
 {
     type Subject = Subject;
 
     fn perform_action(&self, context: &GaContext, population: &mut Population<Self::Subject>) {
         self.inflate(context, population);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+    struct TestSubject;
+    impl Fit<Fitness> for TestSubject {
+        fn measure(&self) -> Fitness {
+            0.0
+        }
+    }
+    impl GaSubject for TestSubject {}
+
+    #[test]
+    fn create_subject_arc() {
+        let create_subject: Arc<dyn Fn(&GaContext) -> TestSubject> =
+            Arc::new(|_: &GaContext| TestSubject);
+        let foo = create_subject.clone();
+        let create_subject: Box<dyn Fn(&GaContext) -> TestSubject> =
+            Box::new(move |ctx: &GaContext| create_subject.clone()(ctx));
+        let inflate = InflateUntilFull(create_subject);
+        let mut population = Population::empty(100);
+        inflate.perform_action(&GaContext::default(), &mut population);
+        assert_eq!(population.subjects.len(), population.pool_size);
+        assert_eq!(foo(&GaContext::default()), TestSubject);
+    }
+
+    #[test]
+    fn create_subject_box() {
+        let create_subject: Box<dyn Fn(&GaContext) -> TestSubject> =
+            Box::new(|_: &GaContext| TestSubject);
+        let inflate = InflateUntilFull(create_subject);
+        let mut population = Population::empty(100);
+        inflate.perform_action(&GaContext::default(), &mut population);
+        assert_eq!(population.subjects.len(), population.pool_size);
+    }
+
+    #[test]
+    fn create_subject_closure() {
+        let create_subject: Box<dyn Fn(&GaContext) -> TestSubject> =
+            Box::new(|_: &GaContext| -> TestSubject { TestSubject });
+        let inflate = InflateUntilFull(create_subject);
+        let mut population = Population::empty(100);
+        inflate.perform_action(&GaContext::default(), &mut population);
+        assert_eq!(population.subjects.len(), population.pool_size);
+    }
+
+    #[test]
+    fn create_subject_fn_pointer() {
+        fn create_subject(_: &GaContext) -> TestSubject {
+            TestSubject
+        }
+        let create_subject: Box<dyn Fn(&GaContext) -> TestSubject> = Box::new(create_subject);
+        let inflate = InflateUntilFull(create_subject);
+        let mut population = Population::empty(100);
+        inflate.perform_action(&GaContext::default(), &mut population);
+        assert_eq!(population.subjects.len(), population.pool_size);
     }
 }
